497b430781a49531c5f3e6780c42a5dfce722649
Brian Floersch <gh123man@gmail.com>
2015-08-23 19:50:40 -0400

    initial commit

---
+++ src/main/java/com/ghsoft/jvcblackhole/Main.java
@@ -0,0 +1,16 @@
+package com.ghsoft.jvcblackhole;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by brian on 8/23/15.
+ */
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finish();
+    }
+}
